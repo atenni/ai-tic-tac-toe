207541f7a7fef7192136fd406d986f1ab50ca368
Andrew T <atenni@users.noreply.github.com>
2022-07-04 16:09:00 +1000

    refactor: Remove unsued js

--- src/js/index.ts
+++
@@ -1,7 +0,0 @@
-import { Game, Player, Board } from './models';
-import handleClick from './controller';
-
-let game = new Game();
-game.init();
-
-document.addEventListener('click', handleClick, { passive: true });
