cdea3684991d3d57548d55fa81731ad78b1b96bb
Andrew T <atenni@users.noreply.github.com>
2022-07-04 16:21:58 +1000

    feat: add initial web component versions

---
+++ src/js/wc/TTTBoard.ts
@@ -0,0 +1,38 @@
+import { TTTSquare } from "./tttSquare.js";
+
+const styles = `
+  section {
+    display: grid;
+    grid-template-areas:
+      ". . ."
+      ". . ."
+      ". . .";
+    gap: 0.5rem;
+
+  }
+`;
+
+class TTTBoard extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = `
+      <style>${styles}</style>
+      <section>
+        <slot name="square"></slot>
+      </section>
+    `;
+  }
+
+  connectedCallback() {
+    const squareElms: NodeListOf<TTTSquare> = this.querySelectorAll(
+      "ttt-square",
+    );
+    for (const [index, elm] of [...squareElms].entries()) {
+      elm.dataset.square = String(index + 1); // Make the board squares 1-indexed
+    }
+  }
+}
+
+customElements.define("ttt-board", TTTBoard);
---
+++ src/js/wc/TTTSquare.ts
@@ -0,0 +1,63 @@
+/**
+ * Element representing one of nine squares on a Tic Tac Toe board.
+ *
+ * Each element has a `value` attribute which is 'x'|'o'|'' (empty).
+ * When clicked, this attribute is updated to the value of the parent #game element's `currentPlayer` attribute.
+ *
+ * The element displays the value of the `value` attribute as a character.
+ */
+
+const styles = `
+button {
+  --square-size: 100px;
+
+  color: var(--gray-800);
+  background: var(--gray-0);
+
+  width: var(--square-size);  /* Might not be necessary if using grid */
+  height: var(--square-size);
+  border: 1px solid black;
+  padding: 0.5rem;
+
+  font-size: calc(var(--square-size) * 0.5);
+
+  transition: background-color 1s; /* Transition to this state */
+}
+
+:host(.winning-square) button {
+  background-color: var(--winning-square-background-color);
+  transition: background-color 1ms; /* Transition to this state */
+}
+`;
+
+export class TTTSquare extends HTMLElement {
+  xoMap: Map<string, string>;
+
+  static get observedAttributes() {
+    return ["value"];
+  }
+
+  constructor() {
+    super();
+
+    this.xoMap = new Map([["x", "✕"], ["o", "◯"]]);
+
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = `
+      <style>${styles}</style>
+      <button type="button"></button>
+    `;
+  }
+
+  attributeChangedCallback(attrName: string, _: string, newVal: string) {
+    if (attrName === "value") {
+      const buttonElm = this.shadowRoot!.querySelector<HTMLButtonElement>(
+        "button",
+      );
+
+      buttonElm!.textContent = this.xoMap.get(newVal) ?? null;
+    }
+  }
+}
+
+customElements.define("ttt-square", TTTSquare);
